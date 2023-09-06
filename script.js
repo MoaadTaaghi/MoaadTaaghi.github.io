e57e5d986b97f8049329fb8dcf626b1cad6c67f7
MED <taaghimoaad@gmail.com>
2023-09-06 20:04:07 +0100

    Added new files

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    let header=document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+   
+header.classList.toggle('sticky', window.scrollY > 100);
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+}
